39cbed41f1db3cecc8762af26eecacb8d985b1e6
Skylar <skylarhimeno@gmail.com>
2019-01-24 08:31:27 -0700

    Created Controller and Runner

---
+++ FirstClass/Controller/Runner.cpp
@@ -0,0 +1,14 @@
+//
+//  Runner.cpp
+//  FirstClass
+//
+//  Created by Himeno, Skylar on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+int main ( )
+{
+    
+    
+    return 0;
+}
